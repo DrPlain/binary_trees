6f45dda07de7dbfae317c2873868e1d4dd68d286
Gideon Obiasor <gideonoiasor@gmail.com>
2022-12-17 01:27:26 +0100

    task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,16 +8,14 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t lheight, rheight;
 
-	if (tree == NULL)
-		return (0);
+	if (tree)
+	{
+		size_t lheight = 0, rheight = 0;
 
-	lheight = binary_tree_height(tree->left);
-	rheight = binary_tree_height(tree->right);
-
-	if (lheight > rheight)
-		return (lheight + 1);
-	else
-		return (rheight + 1);
+		lheight = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		rheight = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((lheight > rheight) ? lheight : rheight);
+	}
+	return (0);
 }
